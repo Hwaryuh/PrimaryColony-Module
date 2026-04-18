b1f965e5cc6e7905e165af026e4a8968a3aa0160
useemeonascreen <hwaryuh@gmail.com>
2026-04-18 18:51:20 +0900

    update: new implements for team packet

--- src/main/java/studio/semicolon/prc/Module.java
+++ src/main/java/studio/semicolon/prc/Module.java
@@ -2,18 +2,22 @@ package studio.semicolon.prc;
 
 import io.quill.paper.Quill;
 import org.bukkit.plugin.java.JavaPlugin;
+import studio.semicolon.prc.api.nms.NMS;
 import studio.semicolon.prc.core.command.CommandRegister;
 import studio.semicolon.prc.core.event.EventRegistrar;
 import studio.semicolon.prc.api.machine.MachineManager;
 import studio.semicolon.prc.core.module.build.BuildSessionManager;
+import studio.semicolon.prc.core.nms.NMSImpl;
 import studio.semicolon.prc.core.server.ServerLinkService;
 
 public final class Module extends JavaPlugin {
     private static Module instance;
+    private static NMS nms;
 
     @Override
     public void onLoad() {
         instance = this;
+        nms = new NMSImpl();
     }
 
     @Override
@@ -22,8 +26,8 @@ public final class Module extends JavaPlugin {
                 new EventRegistrar(),
                 new CommandRegister(),
                 new MachineManager(),
-                BuildSessionManager.getInstance(),
-                new ServerLinkService()
+                new ServerLinkService(),
+                BuildSessionManager.getInstance()
         );
     }
 
@@ -34,5 +38,9 @@ public final class Module extends JavaPlugin {
 
     public static Module getInstance() {
         return instance;
+    }
+
+    public static NMS nms() {
+        return nms;
     }
 }
